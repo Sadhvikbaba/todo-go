21122a9f69c4a4e2e7a43e354e74b18b1607bdd8
“Sadhvikbaba” <psadhvik2006@gmail.com>
2025-04-22 02:36:44 +0530

    final

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:3000",
+		AllowOrigins:     "https://todo-frontend-beta-three.vercel.app",
 		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS,PATCH",
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowCredentials: true,
